4245a6e91b2094d3bdd85355dcb573bda849e075
VadimKorobec <_s_e_t__@ukr.net>
2023-02-27 15:34:41 +0200

    Update RegisterView.js

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 const RegisterView = () => {
-  return <div>RegisterView</div>;
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+  return (
+    <div>
+      <h1>Register View</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+        <label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterView;
