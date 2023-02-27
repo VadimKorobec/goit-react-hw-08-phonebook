50f3aec1d332b163f3488273a382cc65979993ce
VadimKorobec <_s_e_t__@ukr.net>
2023-02-27 14:52:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<HomeView />} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+      </Routes>
+    </div>
+  );
 };
