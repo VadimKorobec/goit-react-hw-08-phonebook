2446c3df7d17a1d1fce671c9f2001a0566116f6d
VadimKorobec <_s_e_t__@ukr.net>
2023-02-27 16:24:50 +0200

    Update RegisterView.js

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,61 +1,10 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import RegisterForm from 'components/RegisterForm/RegisterForm';
 
-const RegisterView = () => {
-  const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleChange = ({ target: { name, value } }) => {
-    switch (name) {
-      case 'name':
-        return setName(value);
-      case 'email':
-        return setEmail(value);
-      case 'password':
-        return setPassword(value);
-      default:
-        return;
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    dispatch(authOperations.register({ name, email, password }));
-    setName('');
-    setEmail('');
-    setPassword('');
-  };
+export const RegisterView = () => {
   return (
     <div>
-      <h1>Register View</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input type="text" name="name" value={name} onChange={handleChange} />
-        </label>
-        <label>
-          Email
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-        </label>
-      </form>
+      <h1>Registration</h1>
+      <RegisterForm />
     </div>
   );
 };
-
-export default RegisterView;
