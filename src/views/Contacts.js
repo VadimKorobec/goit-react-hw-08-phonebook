5a2b8eade2485c9df235d82230706c4fe96d9ad5
VadimKorobec <_s_e_t__@ukr.net>
2023-03-01 17:03:43 +0200

    Update Contacts.js

--- src/views/Contacts.js
+++ src/views/Contacts.js
@@ -1,3 +1,4 @@
+import { ContactList } from 'components/ContactList/ContactList';
 import { ContactsForm } from 'components/ContactsForm/ContactsForm';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -15,6 +16,8 @@ const Contacts = () => {
     <div>
       <h1>Contacts</h1>
       <ContactsForm />
+      <div>{isLoading && 'Request in progress...'}</div>
+      <ContactList />
     </div>
   );
 };
