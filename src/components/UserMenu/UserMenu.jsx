ae31de29bd05e33ff77e526d23b9709834f112ef
VadimKorobec <_s_e_t__@ukr.net>
2023-03-01 16:16:57 +0200

    Create UserMenu.jsx

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,15 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors, authOperations } from 'redux/auth/auth-selectors';
+
+export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUserName);
+  return (
+    <div>
+      <span>Welcome,{name}</span>
+      <button type="button" onClick={() => dispatch(authOperations.logOut())}>
+        Logout
+      </button>
+    </div>
+  );
+};
