d17d4ba2932bf2d3c9e361c205b573f84f5ae36a
VadimKorobec <_s_e_t__@ukr.net>
2023-03-01 17:03:47 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/contacts-operations';
 import { useEffect } from 'react';
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 
-export const Contactlist = () => {
+export const ContactList = () => {
   const contacts = useSelector(selectFilteredContacts);
   const isLoading = useSelector(selectIsloading);
   const error = useSelector(selectError);
@@ -17,7 +18,9 @@ export const Contactlist = () => {
       {isLoading && !error && <b>Request in progress...</b>}
       <ul>
         {contacts.map(contact => (
-          <li key={contact.id}></li>
+          <li key={contact.id}>
+            <ContactListItem contact={contact} />
+          </li>
         ))}
       </ul>
     </div>
