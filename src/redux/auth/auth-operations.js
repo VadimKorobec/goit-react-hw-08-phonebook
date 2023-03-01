c90beead6962ccb5c2e0d727f8bdf46733eb31c0
VadimKorobec <_s_e_t__@ukr.net>
2023-03-01 16:16:55 +0200

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -3,16 +3,34 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com ';
 
-const register = createAsyncThunk('auth/register', async credentials => {
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const register = createAsyncThunk('auth/register', async credentials => {
   try {
     const { data } = await axios.post('/users/signup', credentials);
+    token.set(data.token);
     return data;
   } catch (error) {}
 });
 
-const logIn = createAsyncThunk('autn/login', async credentials => {
+export const logIn = createAsyncThunk('autn/login', async credentials => {
   try {
     const { data } = await axios.post('/users/login', credentials);
+    token.set(data.token);
     return data;
+  } catch (error) {}
+});
+
+export const logOut = createAsyncThunk('auth/logout', async () => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
   } catch (error) {}
 });
