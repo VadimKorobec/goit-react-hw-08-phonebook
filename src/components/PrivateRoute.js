c6af227304a2cade94288955663169b946e52e20
VadimKorobec <_s_e_t__@ukr.net>
2023-03-02 13:58:29 +0200

    Create PrivateRoute.js

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,24 @@
+Routeimport { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { authSelectors } from '../redux/auth';
+
+// export const PrivateRoute = ({
+//   component: Component,
+//   redirectTo,
+//   children,
+//   ...routeProps
+// }) => {
+//   const isloggedIn = useSelector(authSelectors.getIsLoggedIn);
+//   return (
+//     <Route {...routeProps}>
+//     
+//     </Route>
+//   );
+// };
+
+export const PrivateRoute = ({ children, ...routeProps }) => {
+    const isloggedIn = useSelector(authSelectors.getIsLoggedIn);
+    return <Route {...routeProps} >
+          {isloggedIn ? children : <Redirect to='/login' />}
+    </Route>;
+};
