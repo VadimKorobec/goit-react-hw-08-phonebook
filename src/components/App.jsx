175e8f054416e548b9a27a5554653ba45758982c
VadimKorobec <_s_e_t__@ukr.net>
2023-03-02 13:58:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { AppBar } from './AppBar/AppBar';
+import { PrivateRoute } from './PrivateRoute';
 
 const Home = lazy(() => import('../pages/Home'));
 const Register = lazy(() => import('../pages/Register'));
@@ -22,7 +23,10 @@ export const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/contacts" element={<Contacts />} />
+        {/* <Route path="/contacts" element={<Contacts />} /> */}
+        <PrivateRoute patch="/contacts">
+          <Contacts />
+        </PrivateRoute>
       </Routes>
     </div>
   );
