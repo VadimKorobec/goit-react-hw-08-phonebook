7288dc3689916d542e3df2323afc67baaef83f00
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 15:22:54 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,11 @@
+import { AuthNav } from '../AuthNav/AuthNav';
 import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
-import { AuthNav } from '../AuthNav/AuthNav';
+
 import { useAuth } from 'hooks';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
-
   return (
     <header>
       <Navigation />
