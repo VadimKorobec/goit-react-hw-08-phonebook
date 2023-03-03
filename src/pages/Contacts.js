331de48c28537ca221abf1da029b6096401837f4
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 15:50:18 +0200

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet';
 import { Filter } from '../components/Filter/Filter';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Container } from 'components/Container/Container.styled';
 import { ContactsForm } from 'components/ContactsForm/ContactsForm';
 
 export default function Contacts() {
@@ -13,12 +12,12 @@ export default function Contacts() {
         <title>My phonebook</title>
         <meta property="og:title" content="My phonebook" />
       </Helmet>
-      <Container>
+      <div>
         <ContactsForm />
         <Filter />
         <ContactList />
         <ToastContainer position="top-center" theme="colored" />
-      </Container>
+      </div>
     </div>
   );
 }
