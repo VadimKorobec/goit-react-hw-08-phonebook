4c6d54894efdaf7eaaa3ef054449dc3c7098ab02
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 12:10:59 +0200

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,14 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://63f6810f59c944921f75449f.mockapi.io/';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, thunkAPI) => {
     try {
-      const resp = await axios.get('/contacts');
-      return resp.data;
+      const res = await axios.get('/contacts');
+      return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -19,8 +17,8 @@ export const addContact = createAsyncThunk(
   'contacts/addContacts',
   async (contact, thunkAPI) => {
     try {
-      const resp = await axios.post('/contacts', contact);
-      return resp.data;
+      const res = await axios.post('/contacts', contact);
+      return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -31,8 +29,8 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContacts',
   async (id, thunkAPI) => {
     try {
-      const resp = await axios.delete(`/contacts/${id}`);
-      return resp.data;
+      const res = await axios.delete(`/contacts/${id}`);
+      return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
