5b05a92b251f136256b75024522273020b86e0ee
VadimKorobec <107251813+VadimKorobec@users.noreply.github.com>
2023-03-03 12:07:07 +0000

    Deploying to gh-pages from @ VadimKorobec/goit-react-hw-08-phonebook@5e9dc37f48832676e3716501a3c6432a36104ed6 🚀

---
+++ static/js/266.d2806b63.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,a,r){r.r(a);var t=r(184);a.default=function(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Task manager welcome page"})})}}}]);
+//# sourceMappingURL=266.d2806b63.chunk.js.map
