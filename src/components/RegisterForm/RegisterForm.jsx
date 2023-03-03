3302d090081001f969aeb104e97f1b82e3d9e1e7
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 14:42:29 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,9 +4,9 @@ import { register } from 'redux/auth/operations';
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const form = event.currentTarget;
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
     dispatch(
       register({
         name: form.elements.name.value,
@@ -16,23 +16,22 @@ export const RegisterForm = () => {
     );
     form.reset();
   };
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Email
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" />
-        </label>
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <label>
+        Username
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Register</button>
+    </form>
   );
 };
