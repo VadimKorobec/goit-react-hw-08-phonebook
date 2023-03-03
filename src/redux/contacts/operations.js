fc80f8d0ccc43283c453bd773e2dba51ac4dfdd3
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 11:51:55 +0200

    Create operations.js

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://63f6810f59c944921f75449f.mockapi.io/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get('/contacts');
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContacts',
+  async (contact, thunkAPI) => {
+    try {
+      const resp = await axios.post('/contacts', contact);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContacts',
+  async (id, thunkAPI) => {
+    try {
+      const resp = await axios.delete(`/contacts/${id}`);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
