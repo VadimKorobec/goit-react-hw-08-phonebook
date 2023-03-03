2113adf7833d9dab19b18ed4c3f93588926f8bfc
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 15:22:33 +0200

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,17 @@
 import { LoginForm } from 'components/LoginForm/LoginForm';
+import { useAuth } from 'hooks';
+import { Helmet } from 'react-helmet';
 
-const Login = () => {
+export default function Login() {
+  const { isLoadingAuth } = useAuth();
+
   return (
     <div>
-      <h1>Login</h1>
-      <LoginForm />
+      <Helmet prioritizeSeoTags>
+        <title>Login to my phonebook</title>
+        <meta property="og:title" content="Login to my phonebook" />
+      </Helmet>
+      {isLoadingAuth ? <div>...Authorization check</div> : <LoginForm />}
     </div>
   );
-};
-
-export default Login;
+}
