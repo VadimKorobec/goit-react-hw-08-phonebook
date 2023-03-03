5713707b13450df48799ca266c04fbec9b922b6b
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 14:12:29 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
-import { Navigation } from 'components/Navigation/Navigation';
-import { AuthNav } from 'components/AuthNav/AuthNav';
-import { UserMenu } from 'components/UserMenu/UserMenu';
+import { Navigation } from '../Navigation/Navigation';
+import { UserMenu } from '../UserMenu/UserMenu';
+import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
 
 export const AppBar = () => {
