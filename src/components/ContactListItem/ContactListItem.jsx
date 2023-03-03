07916193775575f8bb01ba0a44119ef2e130dbcd
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 12:39:20 +0200

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contacts/operations';
 
 export const ContactListItem = ({ contact }) => {
   const dispatch = useDispatch();
