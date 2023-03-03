87952812d4aa9443471e530a43e7d67b0815c1af
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 15:22:35 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,12 +1,20 @@
 import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+import { useAuth } from 'hooks';
+import { Helmet } from 'react-helmet';
 
-const Register = () => {
+export default function Register() {
+  const { isLoadingAuth } = useAuth();
+
   return (
     <div>
-      <h1>Registration</h1>
-      <RegisterForm />
+      <Helmet prioritizeSeoTags>
+        <title>Registration of a new phonebook user</title>
+        <meta
+          property="og:title"
+          content="Registration of a new phonebook user"
+        />
+      </Helmet>
+      {isLoadingAuth ? <div>...User Registration</div> : <RegisterForm />}
     </div>
   );
-};
-
-export default Register;
+}
