c0434635525ccd38f67083504de0203115162614
VadimKorobec <_s_e_t__@ukr.net>
2023-03-03 12:34:52 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,62 +1,38 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { authOperations } from '../redux/auth';
+import { register } from 'redux/auth/operations';
 
-const RegisterForm = () => {
+export const RegisterForm = () => {
   const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleChange = ({ target: { name, value } }) => {
-    switch (name) {
-      case 'name':
-        return setName(value);
-      case 'email':
-        return setEmail(value);
-      case 'password':
-        return setPassword(value);
-      default:
-        return;
-    }
-  };
 
   const handleSubmit = event => {
     event.preventDefault();
-    dispatch(authOperations.register({ name, email, password }));
-    setName('');
-    setEmail('');
-    setPassword('');
+    const form = event.currentTarget;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>
           Name
-          <input type="text" name="name" value={name} onChange={handleChange} />
+          <input type="text" name="name" />
         </label>
         <label>
           Email
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
+          <input type="email" name="email" />
         </label>
         <label>
           Password
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
+          <input type="password" name="password" />
         </label>
         <button type="submit">Register</button>
       </form>
     </div>
   );
 };
-
-export default RegisterForm;
