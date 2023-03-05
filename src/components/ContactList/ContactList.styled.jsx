b611634dd44de811f3d91c04c3f7f6b2b233680f
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 14:12:27 +0200

    Update ContactList.styled.jsx

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+export const List = styled.ul`
   display: flex;
   flex-direction: column;
   width: 320px;
@@ -13,6 +13,29 @@ export const Container = styled.div`
   margin-top: 50px;
 `;
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 320px;
+  margin: 0 auto;
+  width: 400px;
+  gap: 15px;
+  border-radius: 8px;
+  background-color: #ffefd5;
+  padding: 0px 20px;
+  margin-top: 50px;
+  text-align: center;
+`;
+
 export const Text = styled.p`
-  align-items: center;
+  font-weight: 700;
+  color: #2f4f4f;
+`;
+
+export const Progres = styled.b`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 50px;
 `;
