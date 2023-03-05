f634fd14520f373e9c2d28a468f08ab46acffe0f
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 15:14:54 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -40,7 +40,6 @@ export const logIn = createAsyncThunk(
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await axios.post('/users/logout');
-
     clearAuthHeader();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
