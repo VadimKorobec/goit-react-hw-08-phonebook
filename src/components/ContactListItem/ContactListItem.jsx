fedff5784391578e6c57df047ee54683e9fee31e
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 13:18:16 +0200

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from 'redux/contacts/operations';
+import { Button, Item, ItemText } from './ContactListItem.styled';
 
 export const ContactListItem = ({ contact }) => {
   const { id, name, number } = contact;
@@ -10,13 +11,13 @@ export const ContactListItem = ({ contact }) => {
 
   return (
     <>
-      <li key={id}>
-        <span>{name}</span>
-        <span>{number}</span>
-        <button type="button" onClick={handleDelete}>
+      <Item key={id}>
+        <ItemText>{name}</ItemText>
+        <ItemText>{number}</ItemText>
+        <Button type="button" onClick={handleDelete}>
           Delete
-        </button>
-      </li>
+        </Button>
+      </Item>
     </>
   );
 };
