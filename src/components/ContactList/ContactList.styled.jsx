2f0625bbdd40f5f280942c25df4c79830cb60be3
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 13:04:31 +0200

    Create ContactList.styled.jsx

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const List = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 320px;
+  margin: 0 auto;
+  width: 400px;
+  gap: 15px;
+  border-radius: 8px;
+  background-color: #ffefd5;
+  padding: 20px;
+  margin-top: 50px;
+`;
