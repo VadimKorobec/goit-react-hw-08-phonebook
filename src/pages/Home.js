eb0c97c2e41159bec074edcefc96c334bd90c863
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 11:26:30 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>
-        Contacts book welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          😸
-        </span>
-      </h1>
+      <h1>Contacts book welcome page</h1>
     </div>
   );
 }
