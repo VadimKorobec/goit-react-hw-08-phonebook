6c6786906057075faacb1a5d927738a7c2c6e07a
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 12:38:28 +0200

    Update LoginForm.styled.jsx

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,34 +1,53 @@
 import styled from '@emotion/styled';
 
 export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
   width: 320px;
   margin: 0 auto;
+  width: 400px;
+  gap: 15px;
+  border-radius: 8px;
+  background-color: #ffefd5;
+  padding: 20px;
+  margin-top: 50px;
 `;
 
 export const Label = styled.label`
   display: flex;
   flex-direction: column;
   margin-bottom: 16px;
+  width: 320px;
+  margin: 0 auto;
 `;
 
 export const LabelText = styled.span`
+  font-weight: 700;
+  color: #2f4f4f;
   margin-bottom: 5px;
 `;
 
 export const Input = styled.input`
   display: block;
-  height: 15px;
+  height: 25px;
+  border-radius: 5px;
 `;
 
 export const Button = styled.button`
   display: block;
   margin: 0 auto;
   text-align: center;
-  background-color: #2f4f4f;
+  background-color: #808080;
   padding: 8px;
   font-weight: 700;
-  color: #ff7f50;
+  color: white;
   border-radius: 8px;
   width: 80px;
   cursor: pointer;
+  border: none;
+
+  &:hover,
+  &:focus {
+    background-color: #a9a9a9;
+  }
 `;
