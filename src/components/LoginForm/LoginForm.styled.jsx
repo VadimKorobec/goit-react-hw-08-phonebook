e2d77650526360232e9582a3dd07c7653b53fa5b
VadimKorobec <_s_e_t__@ukr.net>
2023-03-05 12:16:19 +0200

    Create LoginForm.styled.jsx

---
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  width: 320px;
+  margin: 0 auto;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+
+export const LabelText = styled.span`
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  display: block;
+  height: 15px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  background-color: #2f4f4f;
+  padding: 8px;
+  font-weight: 700;
+  color: #ff7f50;
+  border-radius: 8px;
+  width: 80px;
+`;
