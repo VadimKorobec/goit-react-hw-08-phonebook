ac31096350a9b79da461dafc8175057447927a4c
VadimKorobec <_s_e_t__@ukr.net>
2023-03-06 16:10:31 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
+import { Heading } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Contacts book welcome page</h1>
-    </div>
+    <main>
+      <Heading
+        as="h1"
+        size="3xl"
+        textAlign="center"
+        p={20}
+        bg="gray.100"
+        color="#2f4f4f"
+        h="100vh"
+        noOfLines={[1, 2, 3]}
+      >
+        Welcome to the Contact Book
+      </Heading>
+    </main>
   );
 }
