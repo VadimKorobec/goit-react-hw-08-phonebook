1370a5633b98934a0624f048cd67f5e73664c2d2
VadimKorobec <_s_e_t__@ukr.net>
2023-03-06 14:20:12 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,29 @@
-import { useSelector } from 'react-redux';
-import { selectFilteredContacts } from '../../redux/contacts/selectors';
-
-import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import { ContactsListItem } from 'components/ContactListItem/ContactListItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectError, selectIsLoading } from 'redux/contacts/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectFilteredContacts } from 'redux/selectors';
 import { List } from './ContactList.styled';
 
-export const ContactList = () => {
+export const ContactsList = () => {
   const contacts = useSelector(selectFilteredContacts);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <>
+      {isLoading && !error && <b>Request in progress...</b>}
       <List>
         {contacts.map(contact => (
-          <ContactListItem key={contact.id} contact={contact} />
+          <li key={contact.id}>
+            <ContactsListItem contact={contact} />
+          </li>
         ))}
       </List>
     </>
