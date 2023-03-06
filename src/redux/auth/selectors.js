b1863af10a8d3afed45102ca68a605a8ae7366f3
VadimKorobec <_s_e_t__@ukr.net>
2023-03-06 13:38:54 +0200

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,9 +1,5 @@
-export const selectUser = state => state.auth.user;
-
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
+export const selectUser = state => state.auth.user;
+
 export const selectIsRefreshing = state => state.auth.isRefreshing;
-
-export const selectIsLoadingAuth = state => state.auth.isLoadingAuth;
-
-export const selectErrorAuth = state => state.auth.errorAuth;
