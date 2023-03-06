ee7e97ceca0089687257135d59a26eaa9a7144be
VadimKorobec <_s_e_t__@ukr.net>
2023-03-06 14:00:06 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,24 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { setFilter } from 'redux/contacts/selectors';
+import { selectFilterValue } from 'redux/contacts/selectors';
 import { Container, Input, Label, LabelText } from './Filter.styled';
 
 export const Filter = () => {
   const filter = useSelector(state => state.filter);
   const dispatch = useDispatch();
 
-  const handleChange = event => dispatch(setFilter(event.target.value));
+  const handleChange = event => dispatch(selectFilterValue(event.target.value));
 
   return (
     <>
       <Container>
         <Label>
           <LabelText> Find contact by name</LabelText>
-          <Input
-            type="text"
-            name="search"
-            value={filter}
-            onChange={handleChange}
-          />
+          <Input type="text" value={filter} onChange={handleChange} />
         </Label>
       </Container>
     </>
