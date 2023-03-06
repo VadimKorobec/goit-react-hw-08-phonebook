8d31403d77b049695ebd320c6caecd5198cb9cd8
VadimKorobec <_s_e_t__@ukr.net>
2023-03-06 13:44:22 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,40 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectFilteredContacts } from '../../redux/contacts/selectors';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/contacts/operations';
-import {
-  selectErrorContacts,
-  selectIsLoadingContacts,
-} from 'redux/contacts/selectors';
+
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
-import { List, Progres, Text, Wrapper } from './ContactList.styled';
+import { List } from './ContactList.styled';
 
 export const ContactList = () => {
   const contacts = useSelector(selectFilteredContacts);
-  const dispatch = useDispatch();
-  const isLoadingContacts = useSelector(selectIsLoadingContacts);
-  const errorContacts = useSelector(selectErrorContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   return (
     <>
-      {isLoadingContacts && !errorContacts && (
-        <Progres>Request in progress...</Progres>
-      )}
-      {contacts.length ? (
-        <List>
-          {contacts.map(contact => (
-            <ContactListItem key={contact.id} contact={contact} />
-          ))}
-        </List>
-      ) : (
-        <Wrapper>
-          <Text>Start adding contacts!</Text>
-        </Wrapper>
-      )}
+      <List>
+        {contacts.map(contact => (
+          <ContactListItem key={contact.id} contact={contact} />
+        ))}
+      </List>
     </>
   );
 };
