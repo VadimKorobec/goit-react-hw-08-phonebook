99cc894d83979c0a4d36481fd1fac3cc92814721
VadimKorobec <_s_e_t__@ukr.net>
2023-03-06 16:10:39 +0200

    Update ContactList.styled.jsx

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -5,7 +5,7 @@ export const List = styled.ul`
   flex-direction: column;
   width: 320px;
   margin: 0 auto;
-  width: 400px;
+  width: 500px;
   gap: 15px;
   border-radius: 8px;
   background-color: #ffefd5;
@@ -13,29 +13,14 @@ export const List = styled.ul`
   margin-top: 50px;
 `;
 
-// export const Wrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   width: 320px;
-//   margin: 0 auto;
-//   width: 400px;
-//   gap: 15px;
-//   border-radius: 8px;
-//   background-color: #ffefd5;
-//   padding: 0px 20px;
-//   margin-top: 50px;
-//   text-align: center;
-// `;
+export const Item = styled.li`
+  display: block;
+`;
 
-// export const Text = styled.p`
-//   font-weight: 700;
-//   color: #2f4f4f;
-// `;
-
-// export const Progres = styled.b`
-//   display: flex;
-//   flex-direction: column;
-//   margin: 0 auto;
-//   text-align: center;
-//   margin-top: 50px;
-// `;
+export const Progres = styled.b`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 50px;
+`;
